7c2d29431896d1a23dd94c1ded93196c6f0df9a0
Student <nikita.filin228@yandex.ru>
2019-12-25 18:07:05 +0300

    Написана стрельба

--- Lizei/main.py
+++ Lizei/main.py
@@ -1,12 +1,14 @@
 import pygame
 import tkinter as tk
 import math
+
 # я на месте, детка
 root = tk.Tk()
 DISPLAY_SIZE = (root.winfo_screenwidth(), root.winfo_screenheight())
 
 HERO_SPEED = 600 / 1000
 JUMP_SPEED = 800 / 1000
+FIRE_SPEED = 2
 # Загрузка изображений
 projectile = pygame.image.load("Снаряд.png")  # Снаряд героя
 
@@ -49,7 +51,6 @@ class Hero:
 
 
 class GameWorld:
-
     def __init__(self, start_x, start_y):
         self._player = Hero(start_x, start_y)
         self.coor_Picture = 0
@@ -61,11 +62,26 @@ class GameWorld:
         self.angle = 0
         self.y_mouse = 0
         self.x_mouse = 0
+        self.fire_x = 0
+        self.fire_y = 0
+        self.Fire = False
+        self.number_fire = 0
+        self.fire_coordinate = []
+
+    def take_angle(self):
+        return self.angle
 
     def mouse_input_continues(self, event):
         if event.type == pygame.MOUSEMOTION:
             self.x_mouse = event.pos[0]
             self.y_mouse = event.pos[1]
+        # Стрельба, нажатие на клавишу, колличество пуль
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                self.Fire = True
+                self.number_fire += 1
+                self.fire_coordinate.append(
+                    [self._player.get_position()[0] + 40, self._player.get_position()[1] + 48, self.angle])
 
     def process_input_continues(self, pressed_keys, dt):
         if pressed_keys[pygame.K_d]:
@@ -100,6 +116,19 @@ class GameWorld:
             self.A = False
             self.D = False
 
+        # Стрельба, скорость пули, управление координатой, удаление пули, после того, как она вышла за экран игрока
+        i = 0
+        while i != self.number_fire:
+            if self.fire_coordinate[i][0] <= 1.4 * DISPLAY_SIZE[0]:
+                self.fire_coordinate[i][0] += FIRE_SPEED * dt * math.sin(
+                    (self.fire_coordinate[i][2] + 90) * math.pi / 180)
+                self.fire_coordinate[i][1] += FIRE_SPEED * dt * math.cos(
+                    (self.fire_coordinate[i][2] + 90) * math.pi / 180)
+                i += 1
+            else:
+                self.number_fire -= 1
+                del self.fire_coordinate[i]
+
     def rot_center(self, image, angle):
         orig_rect = image.get_rect()
         rot_image = pygame.transform.rotate(image, angle)
@@ -111,10 +140,14 @@ class GameWorld:
     def draw(self, surf):
         Hero = hero_stand
         pl_x, pl_y = self._player.get_position()
-        if self.x_mouse - pl_x - 40 != 0:
-            self.angle = math.atan(-(self.y_mouse - pl_y - 40) / (self.x_mouse - pl_x - 40)) * 180 / math.pi
+        if self.x_mouse - pl_x - 60 != 0:
+            self.angle = math.atan(-(self.y_mouse - pl_y - 50) / (self.x_mouse - pl_x - 60)) * 180 / math.pi
         else:
-            self.angle = math.atan(-(self.y_mouse - pl_y - 40) / 1) * 180 / math.pi
+            self.angle = math.atan(-(self.y_mouse - pl_y - 50) / 1) * 180 / math.pi
+        if math.ceil(self.angle) == 90:
+            self.angle = 88
+        if math.ceil(self.angle) == -89:
+            self.angle = -88
         if not self.D and not self.A:
             Hero = hero_stand
         else:
@@ -130,10 +163,16 @@ class GameWorld:
                 Hero = hero_go_2[self.i]
         surf.blit(bg, (self.coor_Picture, 0))
         surf.blit(Hero, (pl_x - 10, pl_y, 20, 20))
-        screen.blit(self.rot_center(hand, self.angle), (pl_x - 50, pl_y - 50))
+        if self.fire_coordinate != 0:
+            for i in range(self.number_fire):  # отрисова пули
+                surf.blit(projectile, (self.fire_coordinate[i][0], self.fire_coordinate[i][1]))
+        screen.blit(self.rot_center(hand, self.angle), (pl_x - 80, pl_y - 75))
+
+    def take_fire_coordinate(self):
+        return self.fire_coordinate
 
 
-class Monster:
+class Fly_Monster:
     def __init__(self, start_x, start_y):
         self._player = Hero(start_x, start_y)
         self.x_hero = self._player.get_position()[0]
